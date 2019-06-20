7061e2b1d2b71b251141d67c46d5d06f29b5e769
Igor Tkachenko <IATkachenko@gmail.com>
2019-06-20 23:54:55 +0300

    Added command for getting data from Tion S3

--- s3.py
+++ s3.py
@@ -1,5 +1,6 @@
 from . import tion
 
 class s3(tion):
+  getDataCommand = bytearray([61, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90])
   def get() -> dict:
-    return {}
+    return self._decode_response(self._send_request(self.getDataCommand))
