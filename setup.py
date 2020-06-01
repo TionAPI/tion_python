cb28f6f763ec94bc94f632d23ab1265245ee3516
IATkachenko <IATkachenko@gmail.com>
2020-06-01 18:04:37 +0300

    Added setup.sy to use this module via pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tion',
+    version='1.0',
+    packages=find_packages(),
+    long_description="Module for working with Tion breezers",
+)
