d3d3b9b818e4c06ebbddbd55425f2ea416db6d53
IATkachenko <IATkachenko@gmail.com>
2020-12-17 11:37:34 +0300

    fix(tion): Revert "refactor(tion): simplify enabling notifications"
    This reverts commit 047158d2
    It seems that notification stops working in some cases.

--- tion_btle/tion.py
+++ tion_btle/tion.py
@@ -345,6 +345,14 @@ class tion(TionDummy):
 
     def _enable_notifications(self):
         _LOGGER.debug("Enabling notification")
+        setup_data = b"\x01\x00"
+
+        _LOGGER.debug("Notify handler is %s", self.notify.getHandle())
+        notify_handle = self.notify.getHandle() + 1
+
+        _LOGGER.debug("Will write %s to %s handle", setup_data, notify_handle)
+        result = self._btle.writeCharacteristic(notify_handle, setup_data, withResponse=True)
+        _LOGGER.debug("Result is %s", result)
         self._btle.withDelegate(self._delegation)
         _LOGGER.debug("Delegation enabled")
         try:
