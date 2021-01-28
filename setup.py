342dea1d3df741d76755c41593d92609944dfa21
IATkachenko <IATkachenko@gmail.com>
2021-01-28 19:48:58 +0300

    fix: don't get version from consts
    While importing tion_btle.const we had following chain:
    tion_btle.const -> tion_btle -> tion_btle.s3 -> bluepy
    so, had bluepy requirements before processing setup.
    
    tion_btle.const contain just VERSION, that defines at release stage, and we don't use it anywhere.
    
    Should fix https://github.com/TionAPI/HA-tion/issues/36

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from __future__ import print_function
 from setuptools import setup, find_packages
 import os
-import tion_btle.const
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='tion_btle',
-    version=tion_btle.const.VERSION,
+    version='%%%VERSION%%%',
     long_description="Module for working with Tion breezers",
     url='https://github.com/TionAPI/tion_python/tree/dev',
     install_requires=['bluepy==1.3.0'],
