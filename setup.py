1658dd80a8d267ff193b5ea36fe7f99c587109ab
IATkachenko <IATkachenko@gmail.com>
2022-07-08 21:43:22 +0300

    ci: get requirements for module from requirements.txt

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+req = []
+with open("requirements.txt") as f:
+    req.append(f.read())
+
 setup(
     name='tion_btle',
     version='%%%VERSION%%%',
     long_description="Module for working with Tion breezers",
     url='https://github.com/TionAPI/tion_python/tree/dev',
-    install_requires=['bluepy==1.3.0'],
+    install_requires=[req],
     description='Python module for interacting with Tion breezers',
     packages=find_packages(),
 )
