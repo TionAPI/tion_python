6d162a86ddc12f88bb7848a2367dcb85ea15a235
Igor Tkachenko <IATkachenko@gmail.com>
2022-07-08 15:28:48 +0300

    refactor(instance): convert heating to true property

--- tests/unit/tion.py
+++ tests/unit/tion.py
@@ -1,37 +1,23 @@
-import sys
-import unittest
-import unittest.mock as mock
-from unittest.mock import MagicMock
-sys.modules['bluepy'] = MagicMock()
-sys.modules['bluepy.btle'] = MagicMock()
-import tion_btle.tion  # noqa: E402
+import pytest
+
+from tion_btle.tion import tion as Tion
 
 
-class TionTests(unittest.TestCase):
-    def setUp(self):
-        self.patch = mock.patch('tion_btle.tion.tion.__init__', return_value=None)
-        self.patch.start()
-        self._tion = tion_btle.tion.tion("")
+@pytest.mark.parametrize(
+    "heater, heater_temp, in_temp, out_temp, result",
+    [
+        ["on",  20, -2, 21, "on"],
+        ["on",  20, 15, 21, "on"],
+        ["on",  16, 15, 21, "off"],
+        ["off", 20, 15, 21, "off"],
+    ])
+def test__detect_heating_state(heater, in_temp, out_temp, heater_temp, result):
+    """Test heating detection"""
 
-    def tearDown(self):
-        self.patch.stop()
+    tion = Tion(mac="")
+    tion.heater = heater
+    tion._in_temp = in_temp
+    tion._out_temp = out_temp
+    tion._heater_temp = heater_temp
 
-    def test___detect_heating_state(self):
-        # state, in, out, target, heater
-        _variants = [
-            ["on",  -2, 21, 20, "on"],
-            ["on",  15, 21, 20, "on"],
-            ["off", 15, 21, 16, "on"],
-            ["off", 15, 21, 20, "off"],
-        ]
-
-        for expect, in_temp, out_temp, heater_temp, heater in _variants:
-            with self.subTest(expect=expect,
-                              in_temp=in_temp, out_temp=out_temp, heater_temp=heater_temp, heater=heater):
-                # call private __detect_heating_state from tion class
-                self._tion._tion__detect_heating_state(in_temp, out_temp, heater_temp, heater)
-                self.assertEqual(self._tion.heating, expect)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert tion.heating == result
